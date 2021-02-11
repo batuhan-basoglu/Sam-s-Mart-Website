8c2c6b30a78e317f48ac913ef620ff1bc76253c9
soyed <oyediransamuel1995@gmail.com>
2021-02-10 20:22:59 -0500

    changes to lab2 file

--- scripts/groceries.js
+++ scripts/groceries.js
@@ -8,76 +8,91 @@ var products = [
 		vegetarian: true,
 		glutenFree: true,
 		organic: true, 
-		price: 1.99
+		price: 1.99,
+		productImg: "images/broccoli.png" 
 	},
 	{
 		name: "bread",
 		vegetarian: true,
 		glutenFree: false,
 		organic: false, 
-		price: 2.35
+		price: 2.35,
+		productImg: "images/bread.png"
 	},
 	{
 		name: "salmon",
 		vegetarian: false,
 		glutenFree: true,
 		organic: false, 
-		price: 10.00
+		price: 10.00,
+		productImg: "images/salmon.png"
 	},
 	{
 		name: "carrot",
 		vegetarian: true,
 		glutenFree: true,
 		organic: true, 
-		price: 2.63
+		price: 2.63,
+		productImg: "images/carrot.png"
 	},
 	{
 		name: "chicken",
 		vegetarian: false,
 		glutenFree: true,
 		organic: false, 
-		price: 6.87
+		price: 6.87,
+		productImg: "images/chicken.png"
 	},
 	{
 		name: "cereal",
 		vegetarian: false,
 		glutenFree: false,
 		organic: false, 
-		price: 2.97
+		price: 2.97,
+		productImg: "images/cereal.png"
 	},
 	{
 		name: "cheese",
 		vegetarian: false,
 		glutenFree: false,
 		organic: false, 
-		price: 5.14
+		price: 5.14,
+		productImg: "images/cheese.png"
 	},
 	{
 		name: "white fish",
 		vegetarian: false,
 		glutenFree: true,
 		organic: false, 
-		price: 13.50
+		price: 13.50,
+		productImg: "images/white-fish.png"
 	},
 	{
 		name: "honey",
 		vegetarian: false,
 		glutenFree: false,
 		organic: true, 
-		price: 1.89
+		price: 1.89,
+		productImg: "images/honey.png"
 	},
 	{
 		name: "onion",
 		vegetarian: true,
 		glutenFree: true,
 		organic: true, 
-		price: 2.22
+		price: 2.22,
+		productImg: "images/onion.png"
 	}
 ];
 
 products.sort(function(a, b) { 
     return a.price - b.price;
 })
+
+function getProductImg(productName) {
+	var prodVal = products.find(prod => prod.name === productName);
+	return prodVal.productImg;
+}
 
 
 // given restrictions provided, make a reduced list of products
@@ -101,7 +116,7 @@ function restrictListProducts(prods, restriction) {
 		else if ((restriction == "Non-Organic") && (prods[i].organic == false)){
 			product_names.push(prods[i].name);
 		}
-		else if (restriction == "None"){
+		else if ( restriction == "None") {
 			product_names.push(prods[i].name);
 		}
 	}
@@ -110,7 +125,7 @@ function restrictListProducts(prods, restriction) {
 
 // Calculate the total price of items, with received parameter being a list of products
 function getTotalPrice(chosenProducts) {
-	totalPrice = 0;
+	var totalPrice = 0;
 	for (let i=0; i<products.length; i+=1) {
 		if (chosenProducts.indexOf(products[i].name) > -1){
 			totalPrice += products[i].price;
